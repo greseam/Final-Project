d5f801afaba6423fcccde042c558f9d4de58adce
Sean G <greseam@dunwoody.edu>
2019-05-06 13:54:18 -0500

    formating changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,39 +10,24 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.awt.*;
 import java.io.IOException;
 
 
 import javax.swing.*;
+import javax.swing.text.html.ListView;
 
 public class Controller {
 
-    public static int ROUND_LENGTH; // constant time interval for each round, 2 min.
     Main Run = new Main();
 
-    Stage window = new Stage();
     @FXML
     public void Start(ActionEvent actionEvent) throws IOException {
-        window.setTitle("Guess the Animal I am Thinking of!");
-        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("SecondFrame.fxml")), 640, 400));
-        window.show();
-        JOptionPane.showMessageDialog(null,"New window","Guess",2);
-
-        //MainFrame.getChildren().setAll(new AnchorPane(FXMLLoader.load(getClass().getResource("secondframe.fxml"))));
+        Run.generateNewStages();
     }
 
     public void Makeguess(ActionEvent actionEvent) {
-        int keeprunning = 1;
-        while (keeprunning == 1){
-            if (true){
-                JOptionPane.showMessageDialog(null,"Error: Please enter a guess!","Error",2);
-            }
-            else {
-                keeprunning = 0;
-            }
-        }
-        JOptionPane.showMessageDialog(null,"success","",1);
-
+       Run.makeGuess();
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,14 @@
 package sample;
 
 import com.sun.media.jfxmedia.events.NewFrameEvent;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -15,30 +19,68 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.jsoup.nodes.Document;
 
+import javax.swing.*;
+
 public class Main extends Application {
 
     //Main Run = new Main();
+    Stage Rules = new Stage();
+    Stage window = new Stage();
+    Stage Scoreboard = new Stage();
+    @FXML
+    TextField GuessBox;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Rules = primaryStage;
         int Width = 640;
         int Hieght = 350;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        primaryStage.setTitle("Rules!");
-        primaryStage.setScene(new Scene(root, Width, Hieght));
-        primaryStage.show();
+        Rules.setTitle("Rules!");
+        Rules.setScene(new Scene(root, Width, Hieght));
+        Rules.show();
 
-        //set up connection
-
+    }
+    public static void main(String[] args) {
+        launch(args);
 
     }
 
+    //methods to be used in controller
 
+    public String GenerateRandomGuess(){
+        String Name;
+        Name = " ";
+        return Name;
+    }
 
+    public void generateNewStages() throws IOException {
 
-    public static void main(String[] args) {
-        launch(args);
+        window.setTitle("Guess the Animal I am Thinking of!");
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("SecondFrame.fxml")), 640, 400));
+        window.show();
+
+        Scoreboard.setTitle("Scoreboard");
+        Scoreboard.setScene(new Scene(FXMLLoader.load(getClass().getResource("Scoreboard.fxml")), 640, 400));
+        Scoreboard.show();
+    }
+
+    public void makeGuess(){
+        int runOnce = 1;
+        while (runOnce == 1) {
+            int keeprunning = 1;
+            while (keeprunning == 1) {
+                if (GuessBox == null) {
+                    JOptionPane.showMessageDialog(null, "Error: Please enter a guess!", "Error", 2);
+                    keeprunning = 0;
+                } else {
+                    keeprunning = 0;
+                    JOptionPane.showMessageDialog(null, "success", "", 1);
+                }
+            }
+            runOnce--;
+        }
     }
 }
 
